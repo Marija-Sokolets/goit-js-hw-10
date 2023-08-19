4e98d722349542992bcb92fb1bfd8eefa58653ed
Marija-Sokolets <128020732+Marija-Sokolets@users.noreply.github.com>
2023-08-19 18:10:23 +0300

    start01

--- src/js/index.js
+++ src/js/index.js
@@ -8,14 +8,47 @@ import axios from "axios";
 
 axios.defaults.headers.common["x-api-key"] = "live_t1mW2wa0XY9KIPTcSL1wssfIiXpZYy71WPMPSiXAQZC4ztX2kccyRmhGDUPsGF9C";
 
+const refs = {
+    formSelect: document.getElementById("formSelect"),
+    // list: document.getElementById("list"),
+};
 
-fetch("https://api.thecatapi.com/v1/images/search")
-    .then((res) => {
+refs.formSelect.addEventListener("submit", catSelect);
+
+function catSelect(e) {
+    e.preventDefault();
+
+    const breeds = refs.formSelect.elements.breeds;
+    console.log(breeds.value);
+
+    selectCat()
+}
+
+//  в <select> додати <option> = n
+
+function selectCat(breeds) {
+    const URL = "https://api.thecatapi.com/v1/images/search";
+    const API_KEY = "live_t1mW2wa0XY9KIPTcSL1wssfIiXpZYy71WPMPSiXAQZC4ztX2kccyRmhGDUPsGF9C";
+
+    const params = new URLSearchParams({
+        key: API_KEY,
+        // id:"ebv",
+        // url: URL,
+        width: 176, "height": 540,
+        breeds,
+        // favourite:{}
+    });
+
+    return fetch(`${URL}?${params}`).then(res => {
         console.log(res);
         if (!res.ok) {
             throw new Error("Помилка");
+            // throw new Error(res.statusText);
         }
         return res.json();
-    })
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err, "1"));
+    });
+} 
+    selectCat()
+    .then((data) => console.log(data))
+    .catch((err) => console.error(err));
+    
