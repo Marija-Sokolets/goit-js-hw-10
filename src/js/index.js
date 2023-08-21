ecbc041add06aff0028cc7ac72c010011e9a184f
Marija-Sokolets <128020732+Marija-Sokolets@users.noreply.github.com>
2023-08-22 00:01:07 +0300

    breed id

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,6 @@ import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
-// new SlimSelect({
-//   select: '#selectElement'
-// })
-
 const refs = {
     breedSelect: document.getElementById(`.breed-select`),
     catInfo: document.querySelector(`.cat-info`),
@@ -24,46 +20,105 @@ fetchBreeds().then((breeds) => {
 
 refs.breedSelect.addEventListener("submit", catSelect);
 
+function breedSelectId(breeds) {
+    const optionMarkup = breeds.map(({ id, name }, index) =>
+        `<option value="${id}">${name}</option>}`)
+        .join('');
+    
+    refs.breedSelect.insertAdjacentHTML(`afterbegin`, optionMarkup);
+
+    hideLoader();
+
+    new SlimSelect({
+    select: refs.breedSelect
+    })
+
+    showSelect();
+};
+
+
 function catSelect(e) {
     e.preventDefault();
 
-    const breeds = refs.formSelect.elements.breeds;
+    const selectBreed = e.target.value;
     // const { breeds } = e.currentTarget.elements;
     // console.log(breeds.value);
     // console.dir(refs.formSelect.breeds);
+    showLoader();
 
-    selectCat(breeds.value)
+    fetchCatByBreed(selectBreed)
         .then(data => {
-        console.log(data);
+            console.log(data);
+            hideLoader();
+            hideError();
+            refs.catInfo.innerHTML = `<img src="${data[0].url}" alt="" />
+         <div class="characteristics-cat">
+        <h2>${catData[0].breeds[0].name}</h2>
+        <p>${catData[0].breeds[0].description}</p>
+        <p><span class="temp">Temperament: </span>${catData[0].breeds[0].temperament}</p>
+      </div>`;
         })
-    .catch((err) => console.error(err));
+        .catch((err) => {
+            hideLoader();
+            showError();
+            console.error(err);
+        });
+    
+};
+
+function showLoader() {
+    refs.catInfo.innerHTML = ``;
+    refs.loader.computedStyleMap.display = 'block';
+}
+
+function hideLoader() {
+    refs.loader.computedStyleMap.display = 'none';
+}
+
+function showError() {
+    refs.error.classList.remove('hidden');
+}
+
+function hideError() {
+    refs.error.classList.add('hidden');
+}
+
+function showSelect() {
+    refs.selectField.classList.remove('hidden');
+}
+
+function hideSelect() {
+    refs.selectField.classList.add('hidden');
 }
 
 //  в <select> додати <option> = n
 
-function selectCat(breeds) {
-    const URL = "https://api.thecatapi.com/v1/images/search";
-    const API_KEY = "live_t1mW2wa0XY9KIPTcSL1wssfIiXpZYy71WPMPSiXAQZC4ztX2kccyRmhGDUPsGF9C";
+// function catSelect(breeds) {
+//     const URL = "https://api.thecatapi.com/v1/images/search";
 
-    const params = new URLSearchParams({
-        key: API_KEY,
-        // id:"ebv",
-        // url: URL,
-        width: 176, "height": 540,
-        breeds,
-        // favourite:{}
-    });
+//     const params = new URLSearchParams({
+//         // key: API_KEY,
+//         // id:"ebv",
+//         url: URL,
+//         // width: 176, "height": 540,
+//         breeds,
+//         // favourite:{}
+//     });
 
-    return fetch(`${URL}?${params}`).then(res => {
-        console.log(res);
-        if (!res.ok) {
-            throw new Error("Помилка");
-            // throw new Error(res.statusText);
-        }
-        return res.json();
-    });
-} 
+//     return fetch(`${URL}?${params}`).then(res => {
+//         console.log(res);
+//         if (!res.ok) {
+//             throw new Error("Помилка");
+//             // throw new Error(res.statusText);
+//         }
+//         return res.json();
+//     });
+// } 
     // selectCat()
     // .then((data) => console.log(data))
     // .catch((err) => console.error(err));
     
+
+    // new SlimSelect({
+//   select: '#selectElement'
+// })
